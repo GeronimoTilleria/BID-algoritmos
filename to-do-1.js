32b43e82a4662e88119c00883f038dfb31642451
GeronimoTilleria <116458945+GeronimoTilleria@users.noreply.github.com>
2023-05-20 09:50:14 -0400

    3, 4, 5, 6 terminado

--- to-do-1.js
+++ to-do-1.js
@@ -58,9 +58,117 @@ const popAlFrente = arr => {
 
 // INSERTAR EN
 const insertAt = (arr, indice, valor) => {
-    if(indice <= (arr.length - 1) && indice >= 0 ){
-        // Codigo aqui
+    // Comprobamos que el indice va en el rango de la longitud del arreglo
+    if (indice <= (arr.length - 1) && indice >= 0) {
+        // Añadir el valor al final de la matriz si el índice es igual a la longitud de la matriz
+        if (indice === arr.length) {
+            arr.push(valor);
+        } else {
+            // Desplazar los elementos de la matriz a la derecha para hacer espacio para el nuevo elemento
+            for (let i = arr.length - 1; i >= indice; i--) {
+                arr[i + 1] = arr[i];
+            }
+
+            // Insertar el nuevo elemento en el índice especificado
+            arr[indice] = valor;
+        }
     } else {
         console.log('El indice no es correcto...');
     }
 }
+
+// const miArreglo3 = [5, 6, 8, 9, 4];
+// console.log(miArreglo3);
+// insertAt(miArreglo3, 2, 15);
+// console.log(miArreglo3);
+
+// ELIMINA EN
+const removeAt = (arr, indice) => {
+    if(indice <= (arr.length - 1) && indice >= 0){
+
+        if(indice === 0){
+            return popAlFrente(arr);
+        }
+        
+        if(indice === arr.length - 1){
+            let valor = arr[indice];
+            arr.pop();
+            return valor;
+        }
+
+        let eliminado = arr[indice];
+
+        let aux = [];
+        let j = 0;
+
+        for(let i = 0; i < arr.length; i++){
+            if(i !== indice){
+                aux[j] = arr[i];
+                j++;
+            }
+        }
+
+        arr.pop();
+
+        for(let i = 0; i < arr.length; i++){
+            arr[i] = aux[i];
+        }
+
+        return eliminado;
+    }
+    
+    console.log('El indice no es correcto...');
+    return;
+}
+
+//removeAt(arr, 0)
+// const miArreglo4 = [5, 6, 8, 9, 4];
+// console.log(miArreglo4);
+// const eliminado = removeAt(miArreglo4, 4);
+// console.log(miArreglo4);
+// console.log(eliminado);
+
+// Pares de intercambio
+const tradingPairs = arr => {
+    let aux;
+
+    if(arr.length%2 === 0){
+        for(let i = 0; i <= (arr.length / 2); i+=2){
+            aux = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = aux;
+        }
+        return;
+    }
+
+    for(let i = 0; i <= ((arr.length - 1) / 2); i+=2){
+        aux = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = aux;
+    }
+    return;
+};
+
+// const miArreglo5 = [5, 6, 8, 9, 10];
+// console.log(miArreglo5);
+// tradingPairs(miArreglo5);
+// console.log(miArreglo5);
+
+const eliminarDuplicados = arr => {
+    let duplicados = [];
+    // Iteramos sobre la matriz
+    for(let i = 0; i < arr.length; i++) {
+        // Verificamos si el elemento actual es diferente al siguiente elemento
+        if(arr[i] !== arr[i + 1]){
+            // Si son diferentes, agregamos el elemento actual al arreglo sin duplicados
+            duplicados.push(arr[i]);
+        }
+    }
+
+    return duplicados;
+}
+
+const miArreglo6 = [5, 5, 8, 8, 10];
+console.log(miArreglo6);
+const nuevoArreglo = eliminarDuplicados(miArreglo6);
+console.log(nuevoArreglo);
